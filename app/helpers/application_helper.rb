eff80813bcb3aa1450bbd23899a89f8808832225
Cam Fortin <chamberlain.fortin@gmail.com>
2012-02-17 22:51:11 -0800

    fucking around

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
 #Return a title helper on a per page basis
 def full_title (page_title)
-	base_title = "Ruby on Rails Tutorial Sample App"
+	base_title = "Cam's Ruby on Rails Tutorial Sample App"
 	if page_title.empty?
 	 base_title
 	else
