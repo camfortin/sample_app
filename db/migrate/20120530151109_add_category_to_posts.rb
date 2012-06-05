7102150ce6fac788d2d0f8e6d761df2fca1bc026
Cam Fortin <chamberlain.fortin@gmail.com>
2012-06-04 23:25:14 -0700

    user migration

--- db/migrate/20120530151109_add_category_to_posts.rb
+++ db/migrate/20120530151109_add_category_to_posts.rb
@@ -2,10 +2,8 @@ class AddCategoryToPosts < ActiveRecord::Migration
 
         def self.up
             add_column :posts, :category,:string
-            remove_column :posts, :user
         end
         def self.down
-            remove_column :posts,:user_id
             add_column :posts, :category,:string
         end 
 
